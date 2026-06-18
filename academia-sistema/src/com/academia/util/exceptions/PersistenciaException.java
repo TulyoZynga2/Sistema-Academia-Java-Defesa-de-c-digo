26fa3efcd115ca8be7e8843434785dcda3e0a764
TulyoZynga2 <marianosouzaribas@gmail.com>
2026-06-17 22:16:06 -0300

    Update PersistenciaException.java

--- academia-sistema/src/com/academia/util/exceptions/PersistenciaException.java
+++ academia-sistema/src/com/academia/util/exceptions/PersistenciaException.java
@@ -1,9 +1,6 @@
 package com.academia.util.exceptions;
 
-/**
- * Encapsula erros de leitura/gravação de arquivos (I/O) ocorridos
- * durante a serialização ou desserialização dos dados.
- */
+
 public class PersistenciaException extends RuntimeException {
 
     public PersistenciaException(String mensagem, Throwable causa) {
