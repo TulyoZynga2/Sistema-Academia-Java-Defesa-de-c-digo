921c2125e84aea8da05949952b02f4f2b266da3c
TulyoZynga2 <marianosouzaribas@gmail.com>
2026-06-17 22:12:35 -0300

    Update AlunoRepository.java

--- academia-sistema/src/com/academia/repository/AlunoRepository.java
+++ academia-sistema/src/com/academia/repository/AlunoRepository.java
@@ -2,14 +2,14 @@ package com.academia.repository;
 
 import com.academia.model.Aluno;
 
-/** Repositório de Aluno. Herda todo o CRUD/serialização de Repositorio. */
+
 public class AlunoRepository extends Repositorio<Aluno> {
 
     public AlunoRepository() {
         super("alunos.dat");
     }
 
-    /** Consulta extra: verifica se já existe aluno com o CPF informado. */
+    
     public boolean existeCpf(String cpf) {
         for (Aluno aluno : registros) {
             if (aluno.getCpf().equals(cpf)) {
