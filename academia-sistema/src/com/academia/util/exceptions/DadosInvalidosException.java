9e2265375570ba8fcb54ea4c269799f7919e70ce
TulyoZynga2 <marianosouzaribas@gmail.com>
2026-06-17 22:15:45 -0300

    Update DadosInvalidosException.java

--- academia-sistema/src/com/academia/util/exceptions/DadosInvalidosException.java
+++ academia-sistema/src/com/academia/util/exceptions/DadosInvalidosException.java
@@ -1,9 +1,6 @@
 package com.academia.util.exceptions;
 
-/**
- * Lançada quando dados informados pelo usuário não passam nas validações
- * de negócio (ex.: nome vazio, valor negativo, CPF mal formado).
- */
+
 public class DadosInvalidosException extends RuntimeException {
 
     public DadosInvalidosException(String mensagem) {
