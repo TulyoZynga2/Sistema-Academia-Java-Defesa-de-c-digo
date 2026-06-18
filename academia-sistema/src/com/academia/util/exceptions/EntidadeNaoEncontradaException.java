a64e851b9b7de4a28f9df4dab0cee5e7d02cae88
TulyoZynga2 <marianosouzaribas@gmail.com>
2026-06-17 22:15:57 -0300

    Update EntidadeNaoEncontradaException.java

--- academia-sistema/src/com/academia/util/exceptions/EntidadeNaoEncontradaException.java
+++ academia-sistema/src/com/academia/util/exceptions/EntidadeNaoEncontradaException.java
@@ -1,9 +1,6 @@
 package com.academia.util.exceptions;
 
-/**
- * Lançada quando um registro buscado por id não existe no repositório.
- * Também é usada para validar integridade de chave estrangeira.
- */
+
 public class EntidadeNaoEncontradaException extends RuntimeException {
 
     public EntidadeNaoEncontradaException(String mensagem) {
