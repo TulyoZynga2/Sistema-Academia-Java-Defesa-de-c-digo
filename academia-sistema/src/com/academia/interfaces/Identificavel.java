e1c3e6abb042c1541be2d0fc04751c0d809693cc
TulyoZynga2 <marianosouzaribas@gmail.com>
2026-06-17 22:05:55 -0300

    Update Identificavel.java

--- academia-sistema/src/com/academia/interfaces/Identificavel.java
+++ academia-sistema/src/com/academia/interfaces/Identificavel.java
@@ -1,10 +1,6 @@
 package com.academia.interfaces;
 
-/**
- * Contrato que obriga toda entidade persistível a ter um identificador único.
- * É usado pelo {@code Repositorio} genérico para localizar, atualizar e
- * deletar registros sem precisar conhecer o tipo concreto.
- */
+
 public interface Identificavel {
 
     int getId();
